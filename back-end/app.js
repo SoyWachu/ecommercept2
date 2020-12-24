5751e5ddd24f24c5a7d1a0d0aab2b0cf1db4029b
Mauricio Ocando <mauroocando@gmail.com>
2020-12-24 17:25:38 -0300

    actualizacion

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.json());
+app.get("/api/search", async (req, res) => {
+  const query = req.query.producto;
 
-app.get("/sites/MLA/search?q=zapatillas", function (req, res) {
-  console.log(req.query.results);
-  res.send(req.query);
+  const response = await axios.get(
+    `https://api.mercadolibre.com/sites/MLA/search?q=${query}`
+  );
+  const info = response.data.results.map((p) => ({
+    id: p.id,
+    title: p.title,
+    price: p.price,
+    currency_id: p.currency_id,
+    available_quantity: p.available_quantity,
+    img: p.thumbnail,
+    condition: p.condition,
+  }));
+  // console.log(response.data.results);
+  res.send(info);
 });
 
-app.listen(3001, () => {
-  console.log("listening on port 3001");
+app.listen(3000, () => {
+  console.log("listening on port 3000");
 });
